464f6bb78b758c918601ced94525b9386643163b
Masudha Meher <kasturimeh@gmail.com>
2022-03-25 16:18:16 +0530

    feat: side nav

---
+++ src/components/Navbar/sideNav.js
@@ -0,0 +1,17 @@
+export const titleStyle ={
+    backgroundColor: "#212121",
+    color: "#FFCC00",
+    fontFamily: `"Sacramento", cursive`,
+    fontSize: "2.5rem",
+    fontWeight: "700",
+    padding:"0rem 1rem",
+};
+
+export const itemStyle ={
+    // backgroundColor: "#212121",
+    color: "yellow",
+    fontFamily: `"Open Sans", sans-serif`,
+    fontSize: "1rem",
+    padding:"1rem 1rem",
+    margin: "0rem 0rem",
+};
